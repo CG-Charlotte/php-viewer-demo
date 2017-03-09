43d3e7b74c72889173deb412282cdf6c2d18282a
charlotte <charlotte@codingame.com>
2017-03-09 17:07:21 +0100

    viewer test

---
+++ markdowns/lesson1.md
@@ -0,0 +1,8 @@
+# lesson 1
+
+*italic*
+
+
+@[fix this project]({stubs:["www/exo.php"], command:"/bin/sh script.sh"})
+
+test
---
+++ src/script.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+`php -S localhost:8000 -t www
+
+echo CG\> open --port=8000 /project/target/www/ index.php
+
+
+
